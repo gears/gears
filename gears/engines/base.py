76b6626b77fc7cbb760a617bf1477f5ac19db00b
Mike Yumatov <mike@yumatov.org>
2011-11-27 01:33:08 +0400

    Test ExecEngine's process method

--- gears/engines/base.py
+++ gears/engines/base.py
@@ -1,4 +1,4 @@
-from subprocess import Popen, PIPE
+import subprocess
 
 
 class EngineProcessFailed(Exception):
@@ -23,8 +23,11 @@ class ExecEngine(BaseEngine):
             self.executable = executable
 
     def process(self, source, context, calls):
-        args = [self.executable] + self.params
-        p = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        p = subprocess.Popen(
+            args=[self.executable] + self.params,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
         output, errors = p.communicate(input=source)
         if p.returncode == 0:
             return output
--- tests/test_engines_exec.py
+++ tests/test_engines_exec.py
@@ -1,4 +1,6 @@
-from gears.engines.base import ExecEngine
+from __future__ import with_statement
+from gears.engines.base import ExecEngine, EngineProcessFailed
+from mock import Mock, patch
 from unittest2 import TestCase
 
 
@@ -11,3 +13,35 @@ class ExecEnginesTests(TestCase):
 
         self.assertEqual(Engine().executable, 'default')
         self.assertEqual(Engine('custom').executable, 'custom')
+
+    @patch('subprocess.PIPE')
+    @patch('subprocess.Popen')
+    def test_process_success(self, Popen, PIPE):
+        p = Mock()
+        p.returncode = 0
+        p.communicate.return_value = ('output', 'errors')
+        Popen.return_value = p
+
+        class Engine(ExecEngine):
+            executable = 'command'
+            params = ['--compile']
+
+        self.assertEqual(Engine().process('source', {}, set()), 'output')
+        Popen.assert_called_once_with(args=['command', '--compile'],
+                                      stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        p.communicate.assert_called_once_with(input='source')
+
+    @patch('subprocess.PIPE')
+    @patch('subprocess.Popen')
+    def test_process_fail(self, Popen, PIPE):
+        p = Mock()
+        p.returncode = 1
+        p.communicate.return_value = ('output', 'errors')
+        Popen.return_value = p
+
+        class Engine(ExecEngine):
+            executable = 'command'
+            params = ['--compile']
+
+        with self.assertRaisesRegexp(EngineProcessFailed, r'^errors$'):
+            Engine().process('source', {}, set())
