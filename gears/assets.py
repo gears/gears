816bc31a1ad8eaa5282b16b14b606c93a3d56bb9
Mike Yumatov <mike@yumatov.org>
2012-02-12 17:57:16 +0400

    Better circular dependency protection

--- gears/assets.py
+++ gears/assets.py
@@ -1,12 +1,16 @@
 from __future__ import with_statement
 
 from .asset_attributes import AssetAttributes
-from .call_stack import CallStack
 from .utils import cached_property
 
 
-class AssetAlreadyUsed(Exception):
-    pass
+class CircularDependencyError(Exception):
+
+    def __init__(self, absolute_path):
+        self.absolute_path = absolute_path
+
+    def __str__(self):
+        return '%s has already been required' % self.absolute_path
 
 
 class Requirements(object):
@@ -46,7 +50,10 @@ class BaseAsset(object):
     def __init__(self, attributes, absolute_path, calls=None):
         self.attributes = attributes
         self.absolute_path = absolute_path
-        self.calls = calls if calls is not None else CallStack()
+
+        self.calls = calls.copy() if calls else set()
+        if self.absolute_path in self.calls:
+            raise CircularDependencyError(self.absolute_path)
         self.calls.add(self.absolute_path)
 
     def __str__(self):
@@ -101,4 +108,4 @@ def build_asset(environment, path):
     asset_attributes, absolute_path = environment.find(asset_attributes, True)
     if not asset_attributes.processors:
         return StaticAsset(asset_attributes, absolute_path)
-    return Asset(asset_attributes, absolute_path, calls=CallStack())
+    return Asset(asset_attributes, absolute_path)
