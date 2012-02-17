88289eb5cc53f9c96a7e589c4bc110de39806e08
Mike Yumatov <mike@yumatov.org>
2012-02-17 21:48:48 +0400

    Remove unused utils

--- gears/utils.py
+++ gears/utils.py
@@ -30,19 +30,3 @@ def safe_join(base, *paths):
     if not path.startswith(base):
         raise ValueError("Path %r is outside of %r" % (path, base))
     return path
-
-
-def first(function, iterable):
-    if function is None:
-        function = bool
-    for item in iterable:
-        if function(item):
-            return item
-    raise ValueError('No suitable value found.')
-
-
-def first_or_none(function, iterable):
-    try:
-        return first(function, iterable)
-    except ValueError:
-        pass
