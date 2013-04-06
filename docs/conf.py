acf8b325bd9e6ae74c8dcec0da2a2d637037353f
Mike Yumatov <mike@yumatov.org>
2013-04-06 18:47:51 +0400

    2012 -> 2013

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Gears'
-copyright = u'2012, Mike Yumatov'
+copyright = u'2013, Mike Yumatov'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
