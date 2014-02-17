22a857ee7ea30fc9cb74a27ff4c3cd1b075484c0
Mike Yumatov <mike@yumatov.org>
2014-02-17 16:52:46 +0400

    Bump version number for development

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setup(
     name='Gears',
-    version='0.6.1',
+    version='0.7.dev',
     license='ISC',
     description='Compiles and concatenates JavaScript and CSS assets.',
     long_description=read('README.rst'),
