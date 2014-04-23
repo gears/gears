0092906b94b4c8d0d9ab9b30686f6ddd0c21f5c5
Mike Yumatov <mike@yumatov.org>
2014-04-23 09:33:05 +0400

    Raise FileNotFound exception if nothing is found by pattern

--- gears/processors/directives.py
+++ gears/processors/directives.py
@@ -6,6 +6,7 @@ from .base import BaseProcessor
 from ..asset_attributes import AssetAttributes
 from ..assets import Asset
 from ..directives_parser import DirectivesParser
+from ..exceptions import FileNotFound
 
 
 class DirectivesProcessor(BaseProcessor):
@@ -49,11 +50,15 @@ class DirectivesProcessor(BaseProcessor):
                 self.asset.params[key] = value
 
     def process_require_directive(self, path):
+        found = False
         path = self.get_relative_path(path)
         list = self.asset.attributes.environment.list(path, self.asset.attributes.mimetype)
         for asset_attributes, absolute_path in sorted(list, key=lambda x: x[0].path.split('/')):
             self.asset.requirements.add(self.get_asset(asset_attributes, absolute_path))
             self.asset.dependencies.add(os.path.dirname(absolute_path))
+            found = True
+        if not found:
+            raise FileNotFound(path)
 
     def process_require_directory_directive(self, path):
         self.process_require_directive(os.path.join(path, '*'))
@@ -65,10 +70,14 @@ class DirectivesProcessor(BaseProcessor):
         self.asset.requirements.add(self.asset)
 
     def process_depend_on_directive(self, path):
+        found = False
         path = self.get_relative_path(path)
         list = self.asset.attributes.environment.list(path, self.asset.attributes.mimetype)
         for asset_attributes, absolute_path in list:
             self.asset.dependencies.add(absolute_path)
+            found = True
+        if not found:
+            raise FileNotFound(path)
 
     def get_relative_path(self, require_path, is_directory=False):
         require_path = os.path.join(self.asset.attributes.dirname, require_path)
